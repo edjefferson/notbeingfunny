9ee1aa47224e14ce35ecf90ff7b5b79f9c60fed1
edjefferson <edjefferson@reevoo.com>
2013-02-17 17:36:30 +0000

    first commit

---
+++ notbeingfunny.rb
@@ -0,0 +1,35 @@
+require 'twitter'
+require 'mysql'
+
+#heroku config:add DB_HOST= DB_USER= DB_PW= DB_NAME=
+
+#con = Mysql.new ENV['DB_HOST'],ENV['DB_USER'],ENV['DB_PW'],ENV['DB_NAME']
+
+con = Mysql.new 'localhost','weatherman','weatherman','test'
+
+#heroku config:add R_CONSUMER_KEY= R_CONSUMER_SECRET= R_OATH_TOKEN= R_OATH_TOKEN_SECRET=
+
+
+OldTweets = Twitter.configure do |config|
+  config.consumer_key = ENV['R_CONSUMER_KEY']
+  config.consumer_secret = ENV['R_CONSUMER_SECRET']
+  config.oauth_token = ENV['R_OATH_TOKEN']
+  config.oauth_token_secret = ENV['R_OATH_TOKEN_SECRET']
+end
+
+
+
+
+result = con.query("select lasttweet from lasttweet where id=1")
+
+readout = result.fetch_row
+
+
+LatestTweet = NBFTweets.search("\"not being funny\" -RT", :result_type => "recent", :since_id => readout[0].to_i  ).results.reverse.each do |status|
+  
+  puts status.text
+  puts "@#{status.user.username} You are definitely not being funny."
+  puts status.id
+  con.query("update lasttweet set lasttweet=#{status.id} where id=1")
+  
+end
